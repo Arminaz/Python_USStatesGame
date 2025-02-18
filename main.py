205478f8231ad9d8284d356a9586f5b26bef5bab
Arminaz <arminaz321@hotmail.ca>
2025-02-18 04:02:16 -0700

    Added Exit to the Game

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import pandas
 import turtle
 
-# Screen setup
 
 FILE_NAME = "50_states.csv"
 STATE_FONT = ("Courier", 8, "normal")
 
+# Screen setup
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 screen.setup(width=725, height=491)
-
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+# Initialize Variables
 score = 0
 data = pandas.read_csv(FILE_NAME)
+data_states = []
 
 # Get Total States
 total_states = len(data['state'])
 states = data['state']
 
-# State Names
-data_states = []
-
+# While the score is less than total state score.
 while score < total_states:
     answer_state = screen.textinput(title=f"{score}/{total_states} States Correct", prompt="Write a State Name")
+    if answer_state == "Exit":
+        break
+
     correct_state = data[states == answer_state]
-    
     if correct_state.size > 0:
         correct_state = correct_state.iloc[0]
         if correct_state['state'] not in data_states:
